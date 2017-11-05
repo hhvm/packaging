7fb2e3974d863ea4dad72179e9998592c6ed82f8
Fred Emmott <fe@fb.com>
2017-11-05 10:22:27 -0800

    Scripts to get everything checked out on repo publishers

---
+++ ng/aws/userdata/update-repos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+set -ex
+
+shutdown -h 180 # 3 hour timeout
+
+apt-get update -y
+apt-get install -y reprepro awscli
+
+git clone https://github.com/hhvm/packaging hhvm-packaging
+ln -s $(pwd)/hhvm-packaging/ng /opt/hhvm-packaging
+
+
+/opt/hhvm-packaging/aws/bin/update-repos
+
+shutdown -h now
