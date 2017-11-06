72dd16642e60ce641b65bb6e479e265a09bd0364
Fred Emmott <fe@fb.com>
2017-11-06 15:20:13 -0800

    use recent docker to update repos

--- ng/aws/userdata/update-docker.sh
+++ ng/aws/userdata/update-docker.sh
@@ -17,14 +17,26 @@ if [ -z "$VERSION" ]; then
 fi
 
 apt-get update -y
-apt-get install -y docker.io awscli
+apt-get install -y awscli software-properties-common curl apt-transport-https ca-certificates
+
+# We depend on being able to use build arguments in FROM, which requires a very recent Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
+apt-get install -y docker-ce
+
 git clone https://github.com/hhvm/packaging hhvm-packaging
 ln -s $(pwd)/hhvm-packaging/ng /opt/hhvm-packaging
 git clone https://github.com/hhvm/hhvm-docker /opt/hhvm-docker
 
 aws configure set default.region us-west-2
 
-docker login -u hhvmawsbot -p "$(aws kms decrypt --ciphertext-blob 'fileb:///opt/hhvm-packaging/aws/docker-pass.kms-ciphertext' --query Plaintext --output text | base64 --decode)"
+aws kms decrypt \
+  --ciphertext-blob 'fileb:///opt/hhvm-packaging/aws/docker-pass.kms-ciphertext' \
+  --query Plaintext --output text \
+  | base64 --decode \
+  | docker login -u hhvmawsbot --password-stdin
 
 /opt/hhvm-docker/build-and-tag.sh "$VERSION"
 
