13d307e4b8fa8c820fcfad87bdecd79eb860dbe6
Fred Emmott <fe@fb.com>
2017-11-07 13:04:45 -0800

    Update make-source-tarball userdata script to pass through variables from lambda

--- ng/aws/userdata/make-source-tarball.sh
+++ ng/aws/userdata/make-source-tarball.sh
@@ -7,18 +7,26 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-set -ex
-
 shutdown -h 30 # auto-shutdown after 30 minutes
 
+if [ -z "$VERSION" -o -z "$S3_BUCKET" -o -z "$S3_PATH" -o -z "$IS_NIGHTLY" ]; then
+  echo "Must set VERSION, S3_BUCKET, S3_PATH, and IS_NIGHTLY"
+  exit 1
+fi
+
+set -ex
+
 export TZ=UTC
-export VERSION=${VERSION:-"$(date +%Y.%m.%d)"}
+export VERSION
+export IS_NIGHTLY
 
 apt-get update -y
 apt-get install -y curl wget git awscli
 git clone https://github.com/hhvm/packaging hhvm-packaging
 cd hhvm-packaging/ng
+
 bin/make-source-tarball
-aws s3 cp out/*.tar.gz s3://hhvm-downloads/source/nightlies/
+
+aws s3 cp out/*.tar.gz "s3://${S3_BUCKET}/${S3_PATH}"
 
 shutdown -h now
