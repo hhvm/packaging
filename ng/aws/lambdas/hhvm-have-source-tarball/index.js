cf2635cf5840cf27a911e3f5b849f890f7b02284
Fred Emmott <fe@fb.com>
2017-11-07 12:35:42 -0800

    Make hhvm-have-source-tarball take S3 info, and pass through input

--- ng/aws/lambdas/hhvm-have-source-tarball/index.js
+++ ng/aws/lambdas/hhvm-have-source-tarball/index.js
@@ -10,25 +10,21 @@
 const AWS = require('aws-sdk');
  
 exports.handler = (event, context, callback) => {
-  const version = event.version;
-  if (!version) {
-    callback('Version is required');
+  const source = event.source;
+  if (!(source && source.bucket && source.path)) {
+    callback("Input does not include source information");
+    return;
   }
 
-  const nightly = /^\d{4}(\.\d{2}){2}$/.exec(version);
-  const path = nightly
-    ? 'source/nightlies/hhvm-nightly-'+version+'.tar.gz'
-    : 'source/hhvm-'+version+'.tar.gz';
-
   const params = {
-    Bucket: 'hhvm-downloads',
-    Key: path
+    Bucket: source.bucket,
+    Key: source.path
   };
   const s3 = new AWS.S3();
   s3.headObject(params, function(err, data) {
     if (err) {
-      callback(err, params);
+      callback(err, data);
     }
-    callback(null, {version: version, params: params});
+    callback(null, event);
   });
 }
