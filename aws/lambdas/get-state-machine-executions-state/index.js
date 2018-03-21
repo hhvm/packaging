daaf7588408817575173513a054f94cad737f342
Fred Emmott <fe@fb.com>
2018-03-21 15:16:06 -0700

    add lambda to get the status of multiple state machines

---
+++ aws/lambdas/get-state-machine-executions-state/index.js
@@ -0,0 +1,38 @@
+'use strict'
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+const AWS = require('aws-sdk');
+const promise = require('promise');
+
+exports.handler = (event, context, callback) => {
+  const sf = new AWS.StepFunctions();
+
+  event.stepFunctionExecutionStates = {};
+  event.allStepFunctionExecutionsFinished = true;
+
+  promise.all(
+    event.stepFunctionExecutions.map(arn => {
+      return sf.describeExecution({
+        executionArn: arn
+      }).promise();
+    })
+  ).then(results => {
+    results.forEach(function(r) {
+      const arn = r.executionArn;
+      const state = r.status;
+
+      event.stepFunctionExecutionStates[arn] = state;
+      if (state === 'RUNNING') {
+        event.allStepFunctionExecutionsFinished = false;
+      }
+    });
+    callback(null, event);
+  }).done();
+};
