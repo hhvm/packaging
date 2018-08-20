4bc06717c8f69873a38f203e1339308b3404880d
Fred Emmott <fe@fb.com>
2018-08-20 13:44:12 -0700

    Add lambda for more deatiled build status checking
    Example usage (intentionally an unauthenticated API):
    
    ```
    fredemmott-pro:hhvm-packaging fredemmott$ curl https://3gmrqerxm6.execute-api.us-west-2.amazonaws.com/prod/build-status/3.27.2 | jq
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   243  100   243    0     0     82      0  0:00:02  0:00:02 --:--:--    82
    {
      "success": true,
      "version": "3.27.2",
      "succeeded": [
        "macos-sierra",
        "macos-high_sierra",
        "source",
        "source_gpg",
        "debian-8-jessie",
        "debian-9-stretch",
        "ubuntu-14.04-trusty",
        "ubuntu-16.04-xenial",
        "ubuntu-17.10-artful",
        "ubuntu-18.04-bionic"
      ],
      "failed": {}
    }
    fredemmott-pro:hhvm-packaging fredemmott$ curl https://3gmrqerxm6.execute-api.us-west-2.amazonaws.com/prod/build-status/nightly | jq
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   344  100   344    0     0    363      0 --:--:-- --:--:-- --:--:--   363
    {
      "success": false,
      "version": "2018.08.20",
      "succeeded": [
        "macos-sierra",
        "macos-high_sierra",
        "source",
        "source_gpg",
        "debian-8-jessie",
        "debian-9-stretch",
        "ubuntu-16.04-xenial",
        "ubuntu-17.10-artful",
        "ubuntu-18.04-bionic"
      ],
      "failed": {
        "ubuntu-14.04-trusty": "https://dl.hhvm.com/ubuntu/pool/main/h/hhvm-nightly/hhvm-nightly_2018.08.20-1~trusty_amd64.deb"
      }
    }
    ```

---
+++ aws/lambdas/hhvm-get-build-status/index.js
@@ -0,0 +1,91 @@
+'use strict';
+const AWS = require('aws-sdk');
+const promise = require('promise');
+const rp = require('request-promise');
+
+
+function get_version_info(event) {
+  let version = event.version;
+  if (!version) {
+    const s = (new Date()).toISOString()
+    version = s.slice(0,4)+'.'+s.slice(5, 7)+'.'+s.slice(8, 10);
+  }
+  const nightly = !! /^\d{4}(\.\d{2}){2}$/.exec(version);
+  return {
+    nightly,
+    branch: nightly ? 'master' : ('HHVM-'+version.match(/^\d+\.\d+/)[0]),
+    version: version
+  };
+}
+
+function get_distros_uri(branch) {
+  return 'https://raw.githubusercontent.com/hhvm/packaging/'
+    + branch + '/CURRENT_TARGETS';
+}
+
+async function get_distros(branch) {
+  const response = await rp(get_distros_uri(branch));
+  return response.trim().split("\n");
+}
+
+exports.handler = async (event) => {
+  const {nightly, version, branch} = get_version_info(event);
+
+  const prefix = nightly ? 'hhvm-nightly-'+version : 'hhvm-'+version;
+  let paths = {
+    'macos-sierra':
+      'homebrew-bottles/'+prefix+'.sierra.bottle.tar.gz',
+    'macos-high_sierra':
+      'homebrew-bottles/'+prefix+'.high_sierra.bottle.tar.gz',
+  };
+  if (nightly) {
+    paths.source = 'source/nightlies/'+prefix+'.tar.gz';
+    paths.source_gpg = 'source/nightlies/'+prefix+'.tar.gz.sig';
+  } else {
+    paths.source = 'source/'+prefix+'.tar.gz';
+    paths.source_gpg = 'source/'+prefix+'.tar.gz.sig';
+  }
+
+  const distros = await get_distros(branch);
+  distros.forEach(distro => {
+    const debianish = distro.match(/^(ubuntu|debian)/);
+    if (debianish !== null) {
+      paths[distro] = debianish[0] + '/pool/main/h/' +
+        (nightly ? 'hhvm-nightly/hhvm-nightly_' : 'hhvm/hhvm_') +
+        version + '-1~' + (distro.match(/[a-z]+$/)) + '_amd64.deb';
+      return;
+    }
+    // If we add a new distro kind without updating monitoring, make
+    // the monitoring fail.
+    paths[distro] = distro;
+  });
+
+  let success = true;
+  let results = { };
+  const s3 = new AWS.S3();
+  await promise.all(
+    Object.values(paths).map(path => (async function() {
+      try {
+        await s3.headObject(
+          { Bucket: 'hhvm-downloads', Key: path }
+        ).promise();
+        results[path] = true;
+      } catch (err) {
+        results[path] = false;
+        success = false;
+      }
+    })())
+  );
+
+  let response = { success, version, succeeded: [], failed: {} };
+  for (const key in paths) {
+    const path = paths[key];
+    const success = results[path];
+    if (success) {
+      response.succeeded.push(key);
+    } else {
+      response.failed[key] = 'https://dl.hhvm.com/'+path;
+    };
+  }
+  return response;
+}
