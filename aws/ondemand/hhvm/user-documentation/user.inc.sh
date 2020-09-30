007aa607e5408653bb6997880938c7def065e3a2
jjergus <jjergus@fb.com>
2020-09-30 09:36:10 -0700

    aws ondemand: set ulimit for user-documentation

--- aws/ondemand/hhvm/user-documentation/user.inc.sh
+++ aws/ondemand/hhvm/user-documentation/user.inc.sh
@@ -4,6 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# prevents "Too many open files" when running tests
+echo "ulimit -n 1000000" >> /home/ubuntu/.bashrc
+
 cd /home/ubuntu/$REPO
 
 log "Building hhvm/user-documentation (this may take 2 to 3 minutes)..."
