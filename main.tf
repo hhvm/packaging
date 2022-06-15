58dfdd8ec687dfb88b5d9c329d18c9b745c8b9ad
Bo Yang <atry@fb.com>
2022-06-14 18:38:13 -0700

    Format main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ terraform {
     }
   }
   backend "s3" {
-    region = "us-west-2"
-    bucket = "hhvm-packging-terraform-state"
-    key = "hhvm-packging/tfstate"
+    region         = "us-west-2"
+    bucket         = "hhvm-packging-terraform-state"
+    key            = "hhvm-packging/tfstate"
     dynamodb_table = "hhvm-packging-tfstate"
   }
 }
