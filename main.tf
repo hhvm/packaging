d098c8b9c28f529108eb7ee1beae9420be92d215
Yang, Bo <pop.atry@gmail.com>
2022-07-05 18:25:51 +0000

    Revert "Remove resources"
    This reverts commit 3aa869b6792b4d1c398d53a6351ab2412aa89ebe.

--- main.tf
+++ main.tf
@@ -16,3 +16,23 @@ terraform {
     dynamodb_table = "hhvm-packging-tfstate"
   }
 }
+module "base-network" {
+  source                                      = "cn-terraform/networking/aws"
+  name_prefix                                 = "nexus-${terraform.workspace}"
+  vpc_cidr_block                              = "192.168.0.0/16"
+  availability_zones                          = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
+  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
+  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+  single_nat                                  = true
+}
+
+module "nexus" {
+  source              = "cn-terraform/nexus/aws"
+  name_prefix         = "nexus-${terraform.workspace}"
+  region              = "us-west-2"
+  vpc_id              = module.base-network.vpc_id
+  availability_zones  = module.base-network.availability_zones
+  public_subnets_ids  = module.base-network.public_subnets_ids
+  private_subnets_ids = module.base-network.private_subnets_ids
+  enable_s3_logs      = false
+}
